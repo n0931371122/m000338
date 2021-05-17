5754bc2bae3a22878da17aa2037d011014424622
n0931371122 <n0931371122@gmail.com>
2021-05-17 11:26:24 +0800

    header footer

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
-<div class="contact text-white">
-    <div class="max960 text-center">
-        <p class="font-44 NotoSerifTC font-weight-bold mb-0">126~198 坪 磅礡落成，敬邀鑑賞</p>
-        <p class="font-20 NotoSansTC mb-5">接待中心：新北市新莊區新北大道三段163號 / 貴賓專線：02-85211008 </p>
+<div class="contact text-white pt-5">
+    <div class="max960 text-center pt-5 mt-4">
+        <div class="pt-3">
+            <p class="font-44 NotoSerifTC font-weight-bold mb-0">126~198 坪 磅礡落成，敬邀鑑賞</p>
+            <p class="font-20 NotoSansTC mb-5">接待中心：新北市新莊區新北大道三段163號 / 貴賓專線：02-85211008 </p>
+        </div>
         <div class="pt-1">
-            <form action="" class="mt-5">
+            <form action="" class="mt-5 pb-5">
                 <div>
                     <p class="font-65 lh1 mb-2">Contact Us</p>
                     <p class="max476">為了提供您更完善的服務，請務必輸入正確的資料，我們將盡快請專人回覆，謝謝您！ </p>
@@ -13,28 +15,38 @@
                             <input type="text" id="name" name="name" class="border-0 flex-grow text-white">
                         </div>
                         <div class="from-group d-flex align-center bg-white03 mb-3 py-2 px-4">
-                            <label for="tel">電　　話</label>
+                            <label for="tel" class="pr-4">電　　話</label>
                             <input type="text" id="tel" name="tel" class="border-0 flex-grow text-white">
                         </div>
                         <div class="from-group d-flex align-center bg-white03 mb-3 py-2 px-4">
-                            <label for="email">電子信箱</label>
+                            <label for="email" class="pr-4">電子信箱</label>
                             <input type="email" id="email" name="email" class="border-0 flex-grow text-white">
                         </div>
                         <div class="from-group d-flex bg-white03 mb-3 py-2 px-4">
-                            <label for="message" class="mt-1">聯繫內容</label>
+                            <label for="message" class="mt-1 pr-4">聯繫內容</label>
                             <textarea name="message" id="message" class="border-0 flex-grow text-white" rows="3"></textarea>
                         </div>
                         <div class="from-group d-flex align-center bg-white03 mb-3 py-2 px-4">
-                            <label for="verificationCode">驗證碼</label>
-                            <div>
-                                <input type="text" id="verificationCode" name="verificationCode" class="border-0 flex-grow text-white">
+                            <label for="verificationCode" class="pr-4">驗證碼</label>
+                            <div class="flex-grow pr-4">
+                                <input type="text" id="verificationCode" name="verificationCode" class="border-0 text-white">
                             </div>
                             <img src="styles/images/footer/verificationCode.png" alt="">
                         </div>
-                        <button class="font-20 text-white bg-brown py-4" type="submit">預約賞屋</button>
+                        <button class="font-20 text-white bg-brown w-100 py-2" type="submit">
+                            <span class="d-block py-1">預約賞屋</span></button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
+<footer class="Roboto bg-darkBrown text-brown776356 font-16 text-center py-4">
+    <div class="py-3 my-1">
+        <span>Copyright © 2021 Farglory Realty All Rights Reserved.</span>
+        <span>Designed by <a class="text-hover-white" href="https://minmax.tw/" target="_blank">MINMAX 網頁設計</a></span>
+        <span class="ml-4">
+            <a class="text-hover-white" href="">網站使用條款</a><span class="mx-2">|</span><a class="text-hover-white" href="">隱私權政策</a>
+        </span>
+    </div>
+</footer>

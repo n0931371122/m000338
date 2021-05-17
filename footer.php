10952f996ccb29f83ac0aa7ca5069cf92732f278
n0931371122 <n0931371122@gmail.com>
2021-05-17 10:28:29 +0800

    footer

--- footer.php
+++ footer.php
@@ -1,18 +1,18 @@
 <div class="contact text-white">
     <div class="max960 text-center">
-        <p class="font-44 NotoSerifTC">126~198 坪 磅礡落成，敬邀鑑賞</p>
+        <p class="font-44 NotoSerifTC font-weight-bold mb-0">126~198 坪 磅礡落成，敬邀鑑賞</p>
         <p class="font-20 NotoSansTC">接待中心：新北市新莊區新北大道三段163號 / 貴賓專線：02-85211008 </p>
         <form action="">
-            <p class="font-65">Contact Us</p>
+            <p class="font-65 lh1 mb-2">Contact Us</p>
             <p class="max476">為了提供您更完善的服務，請務必輸入正確的資料，我們將盡快請專人回覆，謝謝您！ </p>
             <div class="max580 text-align-left">
-                <div class="from-group bg-white03 mb-3">
-                    <label for="">姓 名</label>
-                    <input type="text" class="border-0">
+                <div class="from-group d-flex bg-white03 mb-3 py-2 px-4">
+                    <label for="">姓　　名</label>
+                    <input type="text" class="border-0 flex-grow">
                 </div>
-                <div class="from-group bg-white03 mb-3">
+                <div class="from-group d-flex bg-white03 mb-3 py-2 px-4">
                     <label for="">姓 名</label>
-                    <input type="text" class="border-0">
+                    <input type="text" class="border-0 flex-grow">
                 </div>
             </div>
         </form>
